fa6b3648ce71c58f0eb2507703c28d8ff84672d6
purpletianjing <tianjingsun@outlook.com>
2016-08-01 11:30:54 +0800

    add border to password and userName

--- microblog/ViewController.swift
+++ microblog/ViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var avaterImageView: UIImageView!
-//    @IBOutlet weak var loginButton: UIButton!
-    
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.userNameTextField.layer.borderWidth = 1
+        self.userNameTextField.layer.cornerRadius = 4
+        self.userNameTextField.layer.borderColor = UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1.0).CGColor
+        self.passwordTextField.layer.borderWidth = 1
+        self.passwordTextField.layer.cornerRadius = 4
+        self.passwordTextField.layer.borderColor = UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1.0).CGColor
         self.avaterImageView.layer.cornerRadius = avaterImageView.frame.size.width/2
         self.avaterImageView.layer.masksToBounds = true
         self.loginButton.backgroundColor = UIColor.orangeColor()
